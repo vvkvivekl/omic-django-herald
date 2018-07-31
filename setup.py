d39f73321a3ec6a2c8e8dab132faaa5298c5aad1
Pranay Majmundar <pranay@revinate.com>
2018-07-31 18:46:49 +0530

    version .2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-VERSION = __import__('herald').__version__ + '.1'
+VERSION = __import__('herald').__version__ + '.2'
 
 
 def read_file(filename):
