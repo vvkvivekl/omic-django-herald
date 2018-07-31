790e9a5bee5da9f93205f08b273664e645747ff9
Pranay Majmundar <pranay@revinate.com>
2018-07-31 20:07:49 +0530

    fix by adding __init__ file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-VERSION = __import__('herald').__version__ + '.2'
+VERSION = __import__('herald').__version__ + '.3'
 
 
 def read_file(filename):
